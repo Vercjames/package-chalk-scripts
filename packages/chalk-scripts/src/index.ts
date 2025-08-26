86e788f4e857063dfc04fe8c735a950733b8e619
James R. Maxfield <Vercjames@gmail.com>
2025-08-25 19:07:29 -0500

    v2.0.0-rc.2

--- packages/chalk-scripts/src/index.ts
+++ packages/chalk-scripts/src/index.ts
@@ -3,17 +3,17 @@
 // =======================================================================================
 import { Script } from "./script"
 import { TSequence, ELogStyle, ELogType } from "./types"
-import { TScriptInputs , TScriptOutputs } from "./scriptIO"
+import { TScriptInputs, TScriptOutputs, IScript } from "./scriptIO"
 
 
 // Application Structure || Export Module
 // =======================================================================================
 // =======================================================================================
-export const scripts = (config?: TScriptInputs): Script => {
+export const scripts = (config?: TScriptInputs): IScript => {
   return new Script(config)
 }
 
 // Application Structure || Export Types
 // =======================================================================================
 // =======================================================================================
-export { ELogStyle, ELogType, TScriptInputs, TScriptOutputs, TSequence }
+export { ELogStyle, ELogType, TScriptInputs, TScriptOutputs, TSequence, IScript }
--- packages/chalk-scripts/src/scriptIO.ts
+++ packages/chalk-scripts/src/scriptIO.ts
@@ -53,6 +53,24 @@ export type TScriptOutputs = {
   timeElapsed: string
 } & TConstructor
 
+// Application Structure || Define Public Interface
+// =======================================================================================
+// =======================================================================================
+export interface IScript {
+  log(...args: any[]): Promise<TScriptOutputs>
+  warn(...args: any[]): Promise<TScriptOutputs>
+  error(...args: any[]): Promise<TScriptOutputs>
+  values(...args: any[]): Promise<TScriptOutputs>
+  extract(...args: any[]): Promise<TScriptOutputs>
+  set(updates: Partial<TScriptInputs>): Promise<void>
+  default(...args: any[]): Promise<TScriptOutputs>
+  success(...args: any[]): Promise<TScriptOutputs>
+  warning(...args: any[]): Promise<TScriptOutputs>
+  failure(...args: any[]): Promise<TScriptOutputs>
+  insight(...args: any[]): Promise<TScriptOutputs>
+  tracker(...args: any[]): Promise<TScriptOutputs>
+}
+
 
 
 
